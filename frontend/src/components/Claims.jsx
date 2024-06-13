9816e9c045db1bb0121b96464781e5751706ceb8
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-06-13 10:13:45 +0200

    insurance-claim

--- frontend/src/components/Claims.jsx
+++ frontend/src/components/Claims.jsx
@@ -34,33 +34,34 @@ security measure called the  Same-Origin Policy. This restricts a web page from 
 than the one it was loaded from. This helps prevent  malicious scripts from stealing data or performing </p>
      </div>
      <div className='images-sub2'>
+      <h2 className='h2'>AVAILABLE INSURANCE CLAIM</h2>
       <div>
-       <img src="./Assets/Images/im5.png" alt="" className='im5'/>
-       <p>Property and Casualty (P&C) Claims: These claims involve damage or loss to property or injuries sustained by a person. They can be further broken down into:
+       <img src="./Assets/Images/im1.png" alt="" className='im1'/>
+       <p><b>Property and Casualty (P&C) Claims:</b> These claims involve damage or loss to property or injuries sustained by a person. They can be further broken down into:
 Homeowner's Insurance Claims,Car Insurance Claims ,Business Interruption Claims</p>
       </div>
       <div>
         <img src="./Assets/Images/im4.png" alt="" className='im4' />
-        <p>Health Insurance Claims: These claims are filed to recover the cost of medical treatment, surgery, hospitalization, or prescription drugs.</p>
+        <p><b>Health Insurance Claims:</b> These claims are filed to recover the cost of medical treatment, surgery, hospitalization, or prescription drugs.</p>
       </div>
       <div>
         <img src="./Assets/Images/im3.png" className='im3' alt="" />
-        <p>Liability Claims: These claims are filed against you by someone else who alleges you caused them injury or property damage.  Your insurance company will typically defend you in court and cover any settlements or judgments up to the policy limits.</p>
+        <p><b>Liability Claims:</b> These claims are filed against you by someone else who alleges you caused them injury or property damage.  Your insurance company will typically defend you in court and cover any settlements or judgments up to the policy limits.</p>
       </div>
       <div>
         <img src="./Assets/Images/im2.png" className='im2' alt="" />
-      <p>Life Insurance Claims: This claim is filed upon the death of the insured person. The beneficiary named in the policy receives the death benefit.</p>
+      <p><b>Life Insurance Claims:</b>This claim is filed upon the death of the insured person. The beneficiary named in the policy receives the death benefit.</p>
       </div>
       <div>
-        <img src="./Assets/Images/im1.png" className='im1' alt="" />
-        <p>Natural Disaster Claims: These claims are filed after a natural disaster such as a hurricane, tornado, or earthquake damages your property.</p>
+        <img src="./Assets/Images/im5.png" className='im5' alt="" />
+        <p><b>Natural Disaster Claims:</b> These claims are filed after a natural disaster such as a hurricane, tornado, or earthquake damages your property.</p>
       </div>
      </div>
       </div>
     <div className='claims'>
-      <h2>Claims</h2>
-      <Link to="/create-claim">Create New Claim</Link>
-      <ul>
+      <h3>create an insurance claim</h3>
+      <Link to="/create-claim"><p className='p'>Create New Claim</p></Link>
+      <ul> 
         {claims.map(claim => (
           <li key={claim._id}>
             {claim.title} - {claim.description} - ${claim.amount}
